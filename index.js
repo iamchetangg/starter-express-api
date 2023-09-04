c3a763373c0138a1aea1f78f6cb9bbcbbe429954
Chetan Gaddam <46570522+iamchetangg@users.noreply.github.com>
2023-09-04 18:09:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ for(var i=0; i<newstr.length; i++){
  map["Description"]=newl[2].replaceAll("Description: ","");
  map["Suitable Camera action"]=newl[3].replaceAll("Suitable Camera action: ","");
     if(newl[4]!==undefined && !newl[4].includes("Transition")){
+        console.log("VO NOT UNDEFINED :: transition = false || " + newl[4].toString());
         map["Brief Voice over Script"]=newl[4].replaceAll("Brief Voice over Script: ","");
     }else if(newl[4]!==undefined && newl[4].includes("Transition")){
+        console.log("VO NOT UNDEFINED :: transition = true || " + newl[4].toString());
           map["Suitable Transition"]=newl[4].replaceAll("Suitable Transition: ","");
     }
  if(newl[5]!==undefined){
+     console.log("TR NOT UNDEFINED"+newl[5].toString());
      map["Suitable Transition"]=newl[5].replaceAll("Suitable Transition: ","");
  }
  newList.push(map);
