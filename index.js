7d69d79614183865a630458cffdf5ab641148d4e
Chetan Gaddam <46570522+iamchetangg@users.noreply.github.com>
2023-09-12 09:55:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.post("/getFrameList", jsonParser, (req, res) => {
   console.log("Just got a request!");
   var input = req.body.content;
   
-  var decodedJSON = JSON.parse(input);
+  var decodedJSON = JSON.parse(JSON.stringify(input));
   console.log(decodedJSON);
    res.send(decodedJSON);
 
